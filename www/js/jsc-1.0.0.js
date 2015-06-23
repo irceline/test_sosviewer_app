c1e84a52a03fef8b1efcb69e5d07d5f6c783c980
Olav Peeters <peeters@irceline.be>
2015-06-24 00:56:59 +0200

    debug config.xml

--- www/js/jsc-1.0.0.js
+++ www/js/jsc-1.0.0.js
@@ -8643,16 +8643,17 @@ var Map = {
                     if (geom[1] < bottommost) {
                         bottommost = geom[1];
                     }
-                    /* customIRCELINE
+                    /* begin customIRCELINE */
                     var LeafIcon = L.Icon.extend({
                     		options: {
                     				iconSize:     [24, 24]
                     		}
                     });
-                    var ircelineIcon = new LeafIcon({iconUrl: 'images/irceline-marker-icon.png'}); */
+                    /* end customIRCELINE */
+                    var ircelineIcon = new LeafIcon({iconUrl: 'images/irceline-marker-icon.png'});
                     var marker = new L.Marker([geom[1], geom[0]], {
-                        id: elem.properties.id /*,
-                        icon: ircelineIcon*/
+                        id: elem.properties.id ,
+                        icon: ircelineIcon // customIRCELINE
                     });
                     marker.on('click', $.proxy(that.markerClicked, that));
                     this.stationMarkers.addLayer(marker);
